1b0b0ed9138fb1309a10ae1d87adfe45e9be93c0
yatharth811 <yatharth.bansiwal@gmail.com>
2023-02-12 03:34:37 +0530

    Added Clock, Part 1 Finished

--- rasterization.cpp
+++ rasterization.cpp
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <iostream>
-#include<vector>
-#include<bits/stdc++.h>
-
+#include <ctime>
+#include <bits/stdc++.h>
 // SDL2
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
@@ -126,8 +125,8 @@ class SoftwareRasterizer{
             float offset = 0.5/(samples);
             int totalSamples = samples*samples;
 
-            int xmin = (int)std::min(t1.x,std::min(t2.x,t3.x)),xmax = (int)std::max(t1.x,std::max(t2.x,t2.x)) + 1;
-            int ymin = (int)std::min(t1.y,std::min(t2.y,t3.y)),ymax = (int)std::max(t1.y,std::max(t2.y,t2.y)) + 1;
+            int xmin = (int)std::min({t1.x, t2.x, t3.x}),xmax = (int)std::max({t1.x, t2.x, t3.x}) + 1;
+            int ymin = (int)std::min({t1.y, t2.y, t3.y}),ymax = (int)std::max({t1.y, t2.y, t3.y}) + 1;
 
             for (int i = xmin; i <= std::min(frameWidth-1,xmax); i++) {
                 for (int j = ymin; j <= std::min(frameHeight-1,ymax); j++){
@@ -193,8 +192,10 @@ class SoftwareRasterizer{
                 clearBuffer(glm::vec4(1.0f));
 
                 // Draw Triangle
-                // createRasterTriangle(glm::vec4(-0.4, -0.8, 0.0, 1.0),glm::vec4(-0.4, -0.4, 0.0, 1.0),glm::vec4(-0.8,  0.0, 0.0, 1.0),glm::vec4(1.0,0.0,0.0,1.0f));
-                drawElements(n,vertices,m,indices,glm::vec4(1.0,0.0,0.0,1.0f));
+                // drawCircle(frameWidth/2, frameHeight/2, 3*(std::min(frameWidth, frameHeight))/8, glm::vec4(0,0,0,1));
+                // createRasterTriangle(glm::vec4(0, 0.01, 0.0, 1.0),glm::vec4(0, -0.01, 0.0, 1.0),glm::vec4(0.75 * std::min(frameWidth, frameHeight)/std::max(frameWidth, frameHeight), 0.0, 0.0, 1.0),glm::vec4(1.0,0.0,0.0,1.0f));
+                // drawElements(n, vertices, m, indices, glm::vec4(1.0,0.0,0.0,1.0f));
+                drawClock();
 
                 // Update screen to apply the changes
                 SDL_BlitScaled(framebuffer, NULL, windowSurface, NULL);
@@ -203,10 +204,94 @@ class SoftwareRasterizer{
 
         }
 
+
+
+        // For rendering clock
+        void drawCircle(int c_x, int c_y, int c_r, glm::vec4 color){
+            color *= 255;
+            Uint32 *pixels = (Uint32*)framebuffer->pixels;
+            float offset = 0.5/(samples);
+            int totalSamples = samples*samples;
+
+            for (int i = 0; i < frameWidth; i++){
+                for (int j = 0; j < frameHeight; j++){
+
+                    Uint8 r, g, b, a;
+                    glm::vec4 sampleColor(0, 0, 0, 0);
+                    SDL_GetRGBA(pixels[i + frameWidth*(frameHeight-1-j)], framebuffer->format, &r, &g, &b, &a);
+                    glm::vec4 previousColor(r,g,b,a);
+
+                    for (int k = 1; k <= samples; k++){
+                        for (int l = 1; l <= samples; l++){
+                            int x = i + (2*k - 1)*offset, y = j + (2*l - 1)*offset;
+
+                            if ((x-c_x)*(x-c_x) + (y-c_y)*(y-c_y) <= c_r*c_r){
+                                sampleColor += color;
+                            } 
+                            else{
+                                sampleColor += previousColor;
+                            }
+                        }
+                    }
+
+                    sampleColor /= totalSamples;
+                    pixels[i + frameWidth*(frameHeight-1-j)] = SDL_MapRGBA(framebuffer->format, sampleColor[0], sampleColor[1], sampleColor[2], sampleColor[3]);
+
+                }
+            }
+        }
+
+        void drawClock(){
+            drawCircle(frameWidth/2, frameHeight/2, (3*(std::min(frameWidth, frameHeight)))/8, glm::vec4(1,1,0,1));
+            time_t now = time(0);
+            tm* ltm = localtime(&now);
+            int hrs = ltm->tm_hour, mins = ltm->tm_min, secs = ltm->tm_sec; 
+            float hl = (0.75 * std::min(frameWidth, frameHeight)) / std::max(frameWidth, frameHeight);
+            float deg_h = 30.0*hrs + 0.5*mins + (1.0/120)*secs, deg_m = 6.0*mins + 0.1*secs, deg_s = 6.0*secs;
+
+            auto transform = [] (float deg) -> glm::mat2x4{
+                glm::mat2x4 res{
+                    glm::cos(glm::radians(deg)), -glm::sin(glm::radians(deg)), 0, 0,
+                    glm::sin(glm::radians(deg)), glm::cos(glm::radians(deg)), 0, 0          
+                };
+                return res;
+            };
+
+            std::cout << hrs << ":" << mins << ":" << secs << std::endl;
+        
+            // glm::vec4 temp = transform(deg_h)*glm::vec4(-0.01, 0.0, 0.0, 1.0);
+            // std::cout << temp[0] << ":" << temp[1] << ":" << temp[2] << std::endl;
+
+            // Hours Hand
+            createRasterTriangle(
+                transform(deg_h)*glm::vec4(-0.015, 0.0, 0.0, 1.0),
+                transform(deg_h)*glm::vec4(0.015, 0.0, 0.0, 1.0),
+                transform(deg_h)*glm::vec4(0.0, 0.8*hl, 0.0, 1.0),
+                transform(deg_h)*glm::vec4(1.0, 0.0, 0.0, 1.0f)
+            );
+
+            // Minute Hand
+            createRasterTriangle(
+                transform(deg_m)*glm::vec4(-0.015, 0.0, 0.0, 1.0),
+                transform(deg_m)*glm::vec4(0.015, 0.0, 0.0, 1.0),
+                transform(deg_m)*glm::vec4(0.0, 0.9*hl, 0.0, 1.0),
+                glm::vec4(0.0, 1.0, 0.0, 1.0f)
+            );
+
+            // Seconds Hand
+            createRasterTriangle(
+                transform(deg_s)*glm::vec4(-0.015, 0.0, 0.0, 1.0),
+                transform(deg_s)*glm::vec4(0.015, 0.0, 0.0, 1.0),
+                transform(deg_s)*glm::vec4(0, hl, 0.0, 1.0),
+                glm::vec4(0.0, 0.0, 1.0, 1.0f)
+            );
+
+        }
+
 };
 
 int main(int argc, char* args[]) {
-    SoftwareRasterizer srz(640,480,1);
+    SoftwareRasterizer srz(1080, 720,1);
     srz.setSamples(16);
     srz.display();
     return 0;
